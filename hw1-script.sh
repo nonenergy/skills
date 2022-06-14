f3566ff436c743e6e5987840fa54f274efc87768
nonenergy <dmbaikalov@ukr.net>
2022-06-14 21:49:29 +0300

    First&second homework

---
+++ hw1-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd C:/vksendzov_course
+mkdir -p hw1/bash/script
+cd hw1/bash/script
+touch gitbash.txt bash.txt script.txt gitbash.json bash.json
+mkdir gitbash bash script
+ls -la
+mv gitbash.txt bash.txt bash
